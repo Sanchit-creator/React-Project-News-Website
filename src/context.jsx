532f3a1866b042245101f666b05743606e9d393a
Sanchit98 <sanchit@novolytics.co>
2023-02-16 21:07:46 +0530

    changed

--- src/context.jsx
+++ src/context.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useReducer } from "react";
 import reducer from "./reducer";
 
 
-let API = "http://hn.algolia.com/api/v1/search?";
+let API = "https://hn.algolia.com/api/v1/search?";
 
 const initialState = {
     isLoading : true,
